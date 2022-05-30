b03b7b0dd6003146c78d5f0dd219096cc58e57ee
dohung13562 <thinhhung1808@gmail.com>
2022-05-30 21:51:52 +0700

    LAB-05

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,35 +1,61 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import Footer from './FooterComponent';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
+import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotion';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+      super(props);
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
+      this.state = {
+          dishes: DISHES,
+          comments: COMMENTS,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
+      };
   }
 
   render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+      const HomePage = () => {
+          return(
+              <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+              />
+          );
+      }
+
+      const DishWithId = ({match}) =>{
+          return(
+              <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+              />
+          );
+      } 
+
+      return (
+          <div>
+              <Header />
+                  <Switch>
+                      <Route path='/home' component={HomePage} />
+                      <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                      <Route path="/menu/:dishId" component={DishWithId} />
+                      <Route exact path='/contactus' component={Contact} />
+                      <Redirect to='/home' />
+                  </Switch>
+              <Footer />
           </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
-    );
+      );
   }
 }
 
