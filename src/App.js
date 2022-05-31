e8ca3a8d9b9dd32cfb45d5c3c80d2757c15acf37
dohung13562 <thinhhung1808@gmail.com>
2022-05-31 23:23:03 +0700

    LAB-11

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
-import Main from './components/MainComponent';
 
 const store = ConfigureStore();
 
@@ -12,11 +12,11 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
-          <div>
-            <Main />
-          </div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
       </Provider>
     );
   }
